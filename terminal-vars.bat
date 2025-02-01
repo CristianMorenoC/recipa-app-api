7f38377b270458d0852cfa4bbefe223632d2ce12
Cristian Moreno <cristianfmoreno95@gmail.com>
2025-01-31 21:28:18 -0500

    added github actions

---
+++ terminal-vars.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set d-ex=docker-compose run --rm app sh -c
+set d-up=docker-compose up
+set d-down=docker-compose down
+set d-build=docker-compose build
+set d-exec=docker-compose exec app
