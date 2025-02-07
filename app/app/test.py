ff594e74d9491c107ce1094c79df07d7795dda6c
Cristian Moreno <cristianfmoreno95@gmail.com>
2025-02-06 20:14:43 -0500

    lint issues fixed 3

--- app/app/test.py
+++ app/app/test.py
@@ -8,11 +8,9 @@ class CalcTest(SimpleTestCase):
 
     """test calc module"""
 
-
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_multiply_numbers(self):
         res = calc.multiply(5, 6)
