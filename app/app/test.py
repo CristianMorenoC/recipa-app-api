f8ab1c536acbfcb4dc1129bbe5db76396fdd3b89
Cristian Moreno <cristianfmoreno95@gmail.com>
2025-02-06 20:06:29 -0500

    lint issues fixed

--- app/app/test.py
+++ app/app/test.py
@@ -1,13 +1,20 @@
+"""sample test"""
+
 from app import calc
 from django.test import SimpleTestCase
 
-"""sample test"""
+
 class CalcTest(SimpleTestCase):
+    
     """test calc module"""
+
+
     def test_add_numbers(self):
       res = calc.add(5, 6)
       self.assertEqual(res, 11)
+
+
     def test_multiply_numbers(self):
       res = calc.multiply(5, 6)
       self.assertEqual(res, 30)
-
+      
